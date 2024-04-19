045ad50c40aee936109012572b382510c680e43c
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-19 12:02:12 +0100

    task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,3 +56,34 @@ class BasicAuth(Auth):
                 return response.decode('utf-8')
             except (binascii.Error, UnicodeDecodeError):
                 return None
+            
+    from typing import Tuple
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """Extract User Credentials
+        
+        Extracts user credentials from a base64-decoded authorization header
+        that uses the Basic authentication flow.
+
+        Args:
+            decoded_base64_authorization_header (str): The decoded base64
+            authorization header.
+
+        Returns:
+            Tuple[str, str]: A tuple containing the extracted
+            username and password.
+                If extraction fails, returns (None, None).
+        """
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None
