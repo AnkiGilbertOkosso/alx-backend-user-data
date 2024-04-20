528e43d3ebb2a8cd1aad55fe6d00bc8dd5d1416b
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-20 11:33:15 +0100

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ Provides session authentication functionality for the API.
 """
 from uuid import uuid4
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -41,3 +42,17 @@ class SessionAuth(Auth):
         """
         if type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """Current User
+
+        Retrieves the user associated with the request.
+
+        Args:
+            request: The Flask request object.
+
+        Returns:
+            User: The user object associated with the request.
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
