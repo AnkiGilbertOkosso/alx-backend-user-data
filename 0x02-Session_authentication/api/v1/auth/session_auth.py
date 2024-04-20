46de52d99b9c8fcd274d3cf53a9811d1cef7ee47
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-20 11:02:18 +0100

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,7 +11,7 @@ class SessionAuth(Auth):
     """Session Authentication Class"""
 
     user_id_by_session_id = {}
-    
+
     def create_session(self, user_id: str = None) -> str:
         """Create Session
 
@@ -27,3 +27,17 @@ class SessionAuth(Auth):
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """User ID for Session ID
+
+        Retrieves the user ID associated with a given session ID.
+
+        Args:
+            session_id (str): The session ID.
+
+        Returns:
+            str: The user ID associated with the session ID.
+        """
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
