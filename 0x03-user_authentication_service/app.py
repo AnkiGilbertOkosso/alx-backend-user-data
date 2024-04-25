4fc1cec8347cade2a6a7321cc6c706616a2292f2
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-25 12:56:18 +0100

    task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -36,5 +36,20 @@ def users() -> Response:
         abort(400)
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """POST /sessions
+    Return:
+        - The account login payload.
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    id = AUTH.create_session(email)
+    res = jsonify({"email": email, "message": "logged in"})
+    res.set_cookie("session_id", id)
+    return res
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
