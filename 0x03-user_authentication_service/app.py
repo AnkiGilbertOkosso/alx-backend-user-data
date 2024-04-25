e7862f65bdda701ef792ca297cc823dd8991bc5a
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-25 12:41:34 +0100

    task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, Response
 
 from auth import Auth
 
@@ -11,7 +11,7 @@ AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
-def index() -> str:
+def index() -> Response:
     """GET /
     Return:
         - The home pages.
@@ -20,11 +20,13 @@ def index() -> str:
 
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
-def users() -> str:
+def users() -> Response:
     """Handle user registration."""
     if request.method == "POST":
-        email, password = request.form.get("email"),
-        request.form.get("password")
+        r_email = request.form.get("email")
+        email = r_email.strip()
+        r_password = request.form.get("password")
+        password = r_password.strip()
         try:
             AUTH.register_user(email, password)
             return jsonify({"email": email, "message": "User created"})
